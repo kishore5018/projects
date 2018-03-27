f7cddd28a534537ffe795666b077f3ff7ff1448d
venkat <venkat@192.168.0.8>
2018-03-27 18:22:13 +0530

    comit

---
+++ tableperheirarchy/src/com/java/conemployee.java
@@ -0,0 +1,19 @@
+package com.java;
+
+public class conemployee extends employee {
+	private float pay_per_hour;
+	private String  contarct_duration;
+	public float getPay_per_hour() {
+		return pay_per_hour;
+	}
+	public void setPay_per_hour(float pay_per_hour) {
+		this.pay_per_hour = pay_per_hour;
+	}
+	public String getContarct_duration() {
+		return contarct_duration;
+	}
+	public void setContarct_duration(String contarct_duration) {
+		this.contarct_duration = contarct_duration;
+	}
+
+}
---
+++ tableperheirarchy/src/com/java/mainapp.java
@@ -0,0 +1,34 @@
+package com.java;
+
+import org.hibernate.*;  
+import org.hibernate.cfg.*;  
+  
+public class mainapp {  
+public static void main(String[] args) {  
+    Session session=new Configuration().configure("hibernate.cfg.xml")  
+                        .buildSessionFactory().openSession();  
+  
+    Transaction t=session.beginTransaction();  
+      
+    employee e1=new employee();  
+    e1.setName("sonoo");  
+      
+    regemployee e2=new regemployee();  
+    e2.setName("Vivek Kumar");  
+    e2.setSalary(50000);  
+    e2.setBonus(5);  
+      
+    conemployee e3=new conemployee();  
+    e3.setName("Arjun Kumar");  
+    e3.setPay_per_hour(1000);  
+    e3.setContarct_duration("15 hours");  
+      
+    session.persist(e1);  
+    session.persist(e2);  
+    session.persist(e3);  
+      
+    t.commit();  
+    session.close();  
+    System.out.println("success");  
+}  
+} 
